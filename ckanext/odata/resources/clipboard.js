d299933bd61ca3fbbb88e01ddba7753381c9f9ff
tobes <toby.junk@gmail.com>
2014-06-09 13:30:44 +0100

    minor change to clipboard js

--- ckanext/odata/resources/clipboard.js
+++ ckanext/odata/resources/clipboard.js
@@ -2,13 +2,13 @@ ckan.module('clipboard', function (jQuery, _) {
   return {
       initialize: function () {
       var $copy = $('<span> <i title="Copy to clipboard" class="icon-paste"></i></span>');
+      this.el.append($copy);
       $copy.clipboard({
         path: '/jquery.clipboard.swf',
         copy: function() {
             alert('Text copied. Try to paste it now!');
             return $(this.el).text();
       }});
-      this.el.append($copy);
     }
   };
 });
