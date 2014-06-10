7fdbf988cb21bcc85eb20cb7d5853fecbeb7d54b
tobes <toby.junk@gmail.com>
2014-06-10 12:01:45 +0100

    cleaner odata endpoint

--- ckanext/odata/resources/clipboard.js
+++ ckanext/odata/resources/clipboard.js
@@ -2,12 +2,10 @@ ckan.module('clipboard', function (jQuery, _) {
   return {
       initialize: function () {
       var el = this.el
-      var $copy = $('<span> <i title="Copy to clipboard" class="icon-paste"></i></span>');
-      el.append($copy);
-      $copy.clipboard({
-        path: '/jquery.clipboard.swf',
-        copy: function() {
-            return el.text();
+      el.clipboard({
+         path: '/jquery.clipboard.swf',
+         copy: function() {
+             return el.text();
       }});
     }
   };
