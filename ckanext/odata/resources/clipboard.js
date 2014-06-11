287a189693ea54c0fdaebe9e17d832f1c3faa333
tobes <toby.junk@gmail.com>
2014-06-11 13:49:32 +0100

    now crawl tree for colors

--- ckanext/odata/resources/clipboard.js
+++ ckanext/odata/resources/clipboard.js
@@ -5,12 +5,24 @@ ckan.module('clipboard', function (jQuery, _) {
       el.clipboard({
          path: '/jquery.clipboard.swf',
          copy: function() {
-            var c1 = el.css('color');
-            var c2 = el.css('background-color');
+            function crawl(el, attr){
+              // find the colour going up the dom as needed
+              var p = el.parent();
+              var v = p.css(attr);
+              if (v != 'rgba(0, 0, 0, 0)' && v != 'transparent'){
+                return v;
+              }
+              if (p.prop("tagName") == 'BODY'){
+                return ('#FFF')
+              };
+              return crawl(p, attr);
+            }
+            var c1 = crawl(el, 'color');
+            var c2 = crawl(el, 'background-color');
             el.css({'color': c2, 'background-color': c1});
             setTimeout( function(){
                 el.css({'color': c1, 'background-color': c2})}
-                , 50);
+                , 100);
              return el.find('.data').text();
       }});
     }
