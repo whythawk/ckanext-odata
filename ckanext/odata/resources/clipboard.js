93fdcbb3612a10cce7af36149986b559c8e3e7ff
tobes <toby.junk@gmail.com>
2014-06-11 11:40:01 +0100

    feedback on click

--- ckanext/odata/resources/clipboard.js
+++ ckanext/odata/resources/clipboard.js
@@ -5,6 +5,12 @@ ckan.module('clipboard', function (jQuery, _) {
       el.clipboard({
          path: '/jquery.clipboard.swf',
          copy: function() {
+            var c1 = el.css('color');
+            var c2 = el.css('background-color');
+            el.css({'color': c2, 'background-color': c1});
+            setTimeout( function(){
+                el.css({'color': c1, 'background-color': c2})}
+                , 50);
              return el.find('.data').text();
       }});
     }
